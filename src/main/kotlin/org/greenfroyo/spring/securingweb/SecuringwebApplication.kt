21f4b46124aba260560a93abc8bd4f0ce902038a
fchristysen <fchristysen@traveloka.com>
2024-02-19 22:55:31 +0700

    init

---
+++ src/main/kotlin/org/greenfroyo/spring/securingweb/SecuringwebApplication.kt
@@ -0,0 +1,11 @@
+package org.greenfroyo.spring.securingweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecuringwebApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecuringwebApplication>(*args)
+}
---
+++ src/test/kotlin/org/greenfroyo/spring/securingweb/SecuringwebApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.greenfroyo.spring.securingweb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecuringwebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
